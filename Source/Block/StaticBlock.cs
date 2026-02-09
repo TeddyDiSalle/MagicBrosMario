b0b6569af293b6e753f91eb6130fad06f3b3ed00
jhuanglululu <huangchuangyun22@gmail.com>
2026-02-08 20:44:36 -0500

    added documentation for static block class

--- Source/Block/StaticBlock.cs
+++ Source/Block/StaticBlock.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace MagicBrosMario.Source.Block;
+
 /// <summary>
 /// block implementation for non-animated and non-updatable blocks
 ///
