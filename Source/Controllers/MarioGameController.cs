a4338aad882b5720aa80af50dea604e016e60036
Teddy <teddydisalle@gmail.com>
2026-02-11 21:38:09 -0500

    Update MarioGameController.cs
    Add all the keybinds, just need the functions now

--- Source/Controllers/MarioGameController.cs
+++ Source/Controllers/MarioGameController.cs
@@ -11,8 +11,10 @@ namespace MagicBrosMario.Source;
 public class MarioGameController : IController{
     //Classes should only worry about game actions not if "w" is pressed
     // This class will call the other's cllasses move functions and such
-    ISprite[] sprites;
-    IController[] keyboardNMouse;
+    private KeyCommandMapper inputMap;
+
+    private ISprite[] sprites;
+    private IController[] keyboardNMouse;
     private int[] cords;
     private MagicBrosMario game;
     public MarioGameController(MagicBrosMario g, ISprite[] Sprites, IController[] Controllers, int[] halfCords )
@@ -21,6 +23,42 @@ public class MarioGameController : IController{
         sprites = Sprites;
         keyboardNMouse = Controllers;
         cords = halfCords;
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        
+        inputMap = new KeyCommandMapper();
+        SetSprint2KeyBinds();
+    }
+
+    public void SetSprint2KeyBinds()
+    {
+        inputMap.Bind(Keys.D0,() => game.Exit());
+        inputMap.Bind(Keys.D1, () => game.SetCurrentSprite(sprites[0]));
+        inputMap.Bind(Keys.D2, () => game.SetCurrentSprite(sprites[1]));
+        inputMap.Bind(Keys.D3, () => game.SetCurrentSprite(sprites[2]));
+        inputMap.Bind(Keys.D4, () => game.SetCurrentSprite(sprites[3]));
+        inputMap.Bind(Keys.W, () => game.Exit()); //TO DO player.jump()
+        inputMap.Bind(Keys.Up, () => game.Exit()); //TO DO player.jump()
+        inputMap.Bind(Keys.A, () => game.Exit()); //TO DO player.left()
+        inputMap.Bind(Keys.Left, () => game.Exit()); //TO DO player.left()
+        inputMap.Bind(Keys.S, () => game.Exit()); //TO DO player.crouch()
+        inputMap.Bind(Keys.Down, () => game.Exit()); //TO DO player.crouch()
+        inputMap.Bind(Keys.D, () => game.Exit()); //TO DO player.right()
+        inputMap.Bind(Keys.Right, () => game.Exit()); //TO DO player.right()
+        inputMap.Bind(Keys.Z, () => game.Exit()); //TO DO player.attack()
+        inputMap.Bind(Keys.N, () => game.Exit()); //TO DO player.attack()
+        inputMap.Bind(Keys.E, () => game.Exit()); //TO DO player.TakeDamage()
+        inputMap.Bind(Keys.T, () => game.Exit()); //TO DO cycle obstacle left
+        inputMap.Bind(Keys.Y, () => game.Exit()); //TO DO cycle obstacle right
+        inputMap.Bind(Keys.U, () => game.Exit()); //TO DO cycle item left/previous
+        inputMap.Bind(Keys.I, () => game.Exit()); //TO DO cycle item right/next
+        inputMap.Bind(Keys.O, () => game.Exit()); //TO DO cycle enemy left/previous
+        inputMap.Bind(Keys.P, () => game.Exit()); //TO DO cycle enemy right/next
+        inputMap.Bind(Keys.Q, () => game.Exit()); 
+        inputMap.Bind(Keys.R, () => game.Exit()); //TO DO reset game
     }
     public void Update()
     {
@@ -29,71 +67,31 @@ public class MarioGameController : IController{
         keyboardInput.Update();
         mouseInput.Update();
         Point position = mouseInput.Position;
+        
+        inputMap.ProcessInput(keyboardInput);
 
 
-        if (keyboardInput.IsKeyDown(Keys.D0) || mouseInput.IsButtonDown(MouseButton.Right))
+        if (mouseInput.IsButtonDown(MouseButton.Right))
         {
             game.Exit();
         }
-        else if (keyboardInput.IsKeyDown(Keys.D1) || (mouseInput.IsButtonDown(MouseButton.Left) && position.X < cords[0] && position.Y < cords[1]))
+        else if (mouseInput.IsButtonDown(MouseButton.Left) && position.X < cords[0] && position.Y < cords[1])
         {
             game.SetCurrentSprite(sprites[0]);
         }
-        else if (keyboardInput.IsKeyDown(Keys.D2) || (mouseInput.IsButtonDown(MouseButton.Left) && position.X > cords[0] && position.Y < cords[1]))
+        else if (mouseInput.IsButtonDown(MouseButton.Left) && position.X > cords[0] && position.Y < cords[1])
         {
             game.SetCurrentSprite(sprites[1]);
         }
-        else if (keyboardInput.IsKeyDown(Keys.D3) || (mouseInput.IsButtonDown(MouseButton.Left) && position.X < cords[0] && position.Y > cords[1]))
+        else if (mouseInput.IsButtonDown(MouseButton.Left) && position.X < cords[0] && position.Y > cords[1])
         {
             game.SetCurrentSprite(sprites[2]);
 
         }
-        else if (keyboardInput.IsKeyDown(Keys.D4) || (mouseInput.IsButtonDown(MouseButton.Left) && position.X > cords[0] && position.Y > cords[1]))
+        else if (mouseInput.IsButtonDown(MouseButton.Left) && position.X > cords[0] && position.Y > cords[1])
         {
             game.SetCurrentSprite(sprites[3]);
         }
     }
     
-    /* Player Controls */
-    private void moveDirection(){
-        // wasd and arrows
-    }
-
-    private void attack()
-    {
-        // mario shoots a fireball with z or n
-    }
-
-    private void takeDamage()
-    {
-        // mario is damaged with e
-    }
-    /* Block/obstacle controls */
-    private void rotateBlock()
-    {
-        // t is previous, y is next
-    }
-
-    /* Item Controls */
-    private void rotateItem(){
-        // press u for previous, 
-        // press i for next
-    }
-
-    /* Enemy/NPC (other chartacter) controls */
-    private void rotateEnemy(){
-        //press o for previous
-        //press p for next
-    }
-
-    /* Other controls */
-    private void quit()
-    {
-        // q
-    }
-
-    private void reset()
-    {
-        // r
-    }
 }
