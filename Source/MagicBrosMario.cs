e770371697d9f50c984738ea96ff4f86a6c6fd6b
Teddy <teddydisalle@gmail.com>
2026-02-16 18:26:41 -0500

    Cleaned up MagicBrosMario
    Cleaning as I go inbetween these merges

--- Source/MagicBrosMario.cs
+++ Source/MagicBrosMario.cs
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 ﻿using MagicBrosMario.Source.Sprite;
-=======
 ﻿using MagicBrosMario.Source.MarioStates;
 using MagicBrosMario.Source.Sprite;
->>>>>>> WorkingOnPlayerState
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -16,159 +13,29 @@ public class MagicBrosMario : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private ISprite currentSprite;
-    private StandstillSprite sprite1;
-    private AnimatedSprite sprite2;
-    private MovingSprite sprite3;
-    private AnimatedMovingSprite sprite4;
-
-    private ISprite[] Sprites;
     private IController Controller;
-=======
-    private KeyboardInfo keyboardInput;
-    private MouseInfo mouseInput;
 
     private MarioStates.Player Mario;
     private SharedTexture texture;
-
->>>>>>> WorkingOnPlayerState
-
-    private Font font;
-=======
-    private SharedTexture sharedTexture;
-    private SharedTexture fireSharedTexture;
-
-    private Goomba goomba;
-    private Koopa koopa;
-    private PiranhaPlant piranhaPlant;
-    private RotatingFireBar rotatingFireBar;
-    private Bowser bowser;
-    private KeyboardState previousKeyboardState;
->>>>>>> WorkingOnEnemies
 
     public MagicBrosMario()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
-<<<<<<< HEAD
         
-    }
-=======
->>>>>>> WorkingOnEnemies
-
-        sharedTexture = new SharedTexture();
-        fireSharedTexture = new SharedTexture();
-        // Goomba
-        var aliveSprite = sharedTexture.NewAnimatedSprite(295, 187, 18, 18, 2, 0.2f);
-        var deadSprite = sharedTexture.NewSprite(276, 187, 18, 18);
-        aliveSprite.Scale = 3f;  
-        deadSprite.Scale = 3f;
-        goomba = new Goomba(
-            aliveSprite,
-            deadSprite,
-            400,    // Y position (moved down from 300)
-            50,     // Left bound (added margin)
-            750     // Right bound (window width minus margin)
-        );
-        // Koopa
-        var koopaWalkingRight = sharedTexture.NewAnimatedSprite(296, 206, 18, 25, 2, 0.2f);
-        var koopaWalkingLeft = sharedTexture.NewAnimatedSprite(182, 206, 18, 25, 2, 0.2f);
-        var koopaShellIdle = sharedTexture.NewSprite(144, 216, 16, 14);
-        var koopaShellMoving = sharedTexture.NewSprite(144, 216, 16, 14);//same as shellIdle for now but it moves
-        var koopaStomped = sharedTexture.NewSprite(163, 215, 16, 15);
-        var koopaShellDeath = sharedTexture.NewSprite(334, 215, 16, 15);
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-        Sprites = new ISprite[] {sprite1, sprite2, sprite3, sprite4};
-        IController[] controllers =  {new KeyboardInfo(), new MouseInfo()};
-        int[] cords = {Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2};
-        Controller = new MarioGameController(this, Sprites, controllers, cords);
-=======
-        koopaWalkingRight.Scale = 3f;
-        koopaWalkingLeft.Scale = 3f;
-        koopaShellIdle.Scale = 3f;
-        koopaShellMoving.Scale = 3f;
-        koopaStomped.Scale = 3f;
-        koopaShellDeath.Scale = 3f;
->>>>>>> WorkingOnEnemies
-=======
-        keyboardInput = new KeyboardInfo();
-        mouseInput = new MouseInfo();
-        
->>>>>>> WorkingOnPlayerState
-
-        koopa = new Koopa(
-            koopaWalkingRight,
-            koopaWalkingLeft,
-            koopaShellIdle,
-            koopaShellMoving,
-            koopaStomped,
-            koopaShellDeath,
-            200,
-            50,
-            750
-        );
-        // Piranha Plant
-        var piranhaAliveSprite = sharedTexture.NewAnimatedSprite(125, 180, 16, 23, 2, 0.2f);
-        piranhaAliveSprite.Scale = 3f;
-
-        piranhaPlant = new PiranhaPlant(
-            piranhaAliveSprite,
-            250,  // X position (pipe location)
-            200   // Y position (top of pipe - adjust based on your screen)
-        );
-        // Rotating Fire Bar
-        rotatingFireBar = new RotatingFireBar(
-            fireSharedTexture,
-            364,    // Fireball X position in sprite sheet
-            188,    // Fireball Y position in sprite sheet
-            8,    // Fireball width
-            8,    // Fireball height
-            400,  // Center X position on screen
-            300,  // Center Y position on screen
-            6,    // Number of fireballs
-            24    // Spacing between fireballs
-        );
-        // Bowser
-        var bowserWalkingRight = sharedTexture.NewAnimatedSprite(255, 368, 35, 32, 4, 0.2f);
-        var bowserWalkingLeft = sharedTexture.NewAnimatedSprite(116, 368, 35, 32, 2, 0.2f);  // Left animation
-
-        bowserWalkingRight.Scale = 3f;
-        bowserWalkingLeft.Scale = 3f;
-
-        bowser = new Bowser(
-            bowserWalkingRight,
-            bowserWalkingLeft,
-            fireSharedTexture,
-            161,   // Fire moving right - X coordinate (2 frames)
-            253,   // Fire moving right - Y coordinate
-            101,    // Fire moving left - X coordinate (2 frames)
-            253,    // Fire moving left - Y coordinate
-            24,             // Fire width
-            8,             // Fire height
-            100,            // Bowser Y position
-            50,             // Left bound
-            750             // Right bound
-        );
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-<<<<<<< HEAD
         Texture2D _texture = Content.Load<Texture2D>("characters");
         Texture2D _fireTexture = Content.Load<Texture2D>("enemies");
 
         sharedTexture.BindTexture(_texture);
         
         fireSharedTexture.BindTexture(_fireTexture);
-=======
         SpriteFont fontDesc = Content.Load<SpriteFont>("font");
         Texture2D characterSheet = Content.Load<Texture2D>("characters");
         Texture2D MarioSheet = Content.Load<Texture2D>("MarioStarSheet");
@@ -178,57 +45,16 @@ public class MagicBrosMario : Game
         Mario = new Player(texture);
         Debug.WriteLine(halfX +  " " + halfY);
 
->>>>>>> WorkingOnPlayerState
     }
 
-    protected override void Update(GameTime gameTime)
-{
-    KeyboardState currentKeyboardState = Keyboard.GetState();
-
-    // Check if K was just pressed (not held)
-    if (currentKeyboardState.IsKeyDown(Keys.K) && previousKeyboardState.IsKeyUp(Keys.K))
-    {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        Controller.Update();
-=======
-        //Code
->>>>>>> WorkingOnPlayerState
-
-            InputCheck(gameTime);
-
-        Mario.Update(gameTime);
-        base.Update(gameTime);
-=======
-        goomba.Kill();
-        koopa.Kill();
-        piranhaPlant.Kill();
-        bowser.Kill();
->>>>>>> WorkingOnEnemies
-    }
-
-    // Check if M was just pressed (not held)
-    if (currentKeyboardState.IsKeyDown(Keys.M) && previousKeyboardState.IsKeyUp(Keys.M))
-    {
-        koopa.KickShell(true);
-    }
-
-    goomba.Update(gameTime);
-    koopa.Update(gameTime);
-    piranhaPlant.Update(gameTime);
-    rotatingFireBar.Update(gameTime);
-    bowser.Update(gameTime);
-
-    previousKeyboardState = currentKeyboardState;
-
-    base.Update(gameTime);
+    protected override void Update(GameTime gameTime){
+  
 }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-<<<<<<< HEAD
         _spriteBatch.Begin();
 
         goomba.Draw(_spriteBatch);
@@ -237,83 +63,12 @@ public class MagicBrosMario : Game
         rotatingFireBar.Draw(_spriteBatch);
         bowser.Draw(_spriteBatch);
 
-=======
         //Code
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
         Mario.Draw(_spriteBatch);
->>>>>>> WorkingOnPlayerState
         _spriteBatch.End();
 
         base.Draw(gameTime);
     }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public void SetCurrentSprite(ISprite sprite)
-    {
-        currentSprite = sprite;
-=======
-    private void InputCheck(GameTime gameTime)
-    {
-        keyboardInput.Update();
-        mouseInput.Update();
-        Point position = mouseInput.Position;
-
-
-        if (keyboardInput.IsKeyDown(Keys.D0))
-        {
-            Exit();
-        }
-        else if (keyboardInput.IsKeyDown(Keys.W))
-        {
-            Mario.Jump(gameTime);
-        }
-        else if (keyboardInput.IsKeyDown(Keys.A))
-        {
-            Mario.Left(gameTime);
-        }
-        else if (keyboardInput.IsKeyDown(Keys.D))
-        {
-            Mario.Right(gameTime);
-        }
-        else if (keyboardInput.IsKeyDown(Keys.S))
-        {
-            Mario.Crouch(gameTime);
-        }
-        else if (keyboardInput.IsKeyDown(Keys.X))
-        {
-            Mario.TakeDamage();
-        }
-        else if (keyboardInput.IsKeyDown(Keys.F))
-        {
-            Mario.Attack();
-        }
-        else if (keyboardInput.IsKeyDown(Keys.D1))
-        {
-            Mario.ChangeState(new SmallMarioIdleState(Mario, texture, 0.15, 3));
-        }
-        else if (keyboardInput.IsKeyDown(Keys.D2))
-        {
-            Mario.ChangeState(new BigMarioIdleState(Mario, texture, 0.15, 3));
-        }
-        else if (keyboardInput.IsKeyDown(Keys.D3))
-        {
-            Mario.PowerUp(Power.FireFlower);
-        }
-        else if (keyboardInput.IsKeyDown(Keys.D4))
-        {
-            Mario.PowerUp(Power.Star);
-        }
-        else
-        {
-            Mario.ReleaseCrouch();
-            Mario.Idle();
-        }
-       
->>>>>>> WorkingOnPlayerState
-    }
 }
 
-=======
-}
->>>>>>> WorkingOnEnemies
