0441a8071794043a609ebd12264b6e0b6b57d2ae
Teddy <teddydisalle@gmail.com>
2026-02-16 20:45:13 -0500

    Have an enviroment similar to what sprint 2 needs
    Unfinished, almost everybody is on screen. Still need blocks. need all the enemies in the same spot. Plenty of bugs.
      Star power doesn't go away
      Can't go left
      Switching enemies crashes the game
    I am now going to submit the sprint2functionality check.

--- Source/MagicBrosMario.cs
+++ Source/MagicBrosMario.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 
 namespace MagicBrosMario.Source;
@@ -13,62 +14,176 @@ public class MagicBrosMario : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private IController Controller;
+    private MarioGameController Controller;
 
-    private MarioStates.Player Mario;
+    public MarioStates.Player Mario;
     private SharedTexture texture;
+
+    private int enemyArraySize = 6;
+    private int blockArraySize = 4;
+    private int enemyIndex = 0;
+    private int blockIndex = 0;
+    private IEnemy[] enemy;
+    private IBlock[] block;
 
     public MagicBrosMario()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
-        
+       enemy = new IEnemy[enemyArraySize];
+       block = new IBlock[blockArraySize];
     }
 
-    protected override void LoadContent()
-    {
+    protected override void LoadContent(){
+
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        Texture2D _texture = Content.Load<Texture2D>("characters");
-        Texture2D _fireTexture = Content.Load<Texture2D>("enemies");
-
-        sharedTexture.BindTexture(_texture);
         
-        fireSharedTexture.BindTexture(_fireTexture);
-        SpriteFont fontDesc = Content.Load<SpriteFont>("font");
-        Texture2D characterSheet = Content.Load<Texture2D>("characters");
+        LoadEnemies();
+        LoadBlocks();
+
+        Content.Load<SpriteFont>("font");
+        Content.Load<Texture2D>("characters");
         Texture2D MarioSheet = Content.Load<Texture2D>("MarioStarSheet");
         texture = new SharedTexture();
         texture.BindTexture(MarioSheet);
 
         Mario = new Player(texture);
-        Debug.WriteLine(halfX +  " " + halfY);
+
+        MarioGameController.Sprint2Controller data = new MarioGameController.Sprint2Controller{
+            player = Mario,
+            mouse = new MouseInfo(),
+            keyb = new KeyboardInfo(),
+            halfX = _graphics.PreferredBackBufferWidth / 2,
+            halfY = _graphics.PreferredBackBufferHeight / 2
+            };
+        Controller = new MarioGameController(this, ref data);
+    }
+
+// Make sure texture is set to characters
+    private void LoadEnemies()
+    {   
+        texture = new SharedTexture();
+        texture.BindTexture(Content.Load<Texture2D>("characters"));
+        SharedTexture fireSharedTexture = new SharedTexture();
+        fireSharedTexture.BindTexture(Content.Load<Texture2D>("enemies"));
+        // initialize enemies
+        // all the sprite scales have to be set to 3f, bruh hooooooow
+        enemy[0] = new Goomba(
+            texture.NewAnimatedSprite(295, 187, 18, 18, 2, 0.2f), // alive
+            texture.NewSprite(276, 187, 18, 18), // or maybe dead
+            400,
+            50,
+            750
+        );
+        enemy[1] = new Koopa(
+            texture.NewAnimatedSprite(296, 206, 18, 25, 2, 0.2f), // walkling right
+            texture.NewAnimatedSprite(182, 206, 18, 25, 2, 0.2f), // walking left
+            texture.NewSprite(144, 216, 16, 14), //  shell idle
+            texture.NewSprite(144, 216, 16, 14), // repeate of shell idle
+            texture.NewSprite(163, 215, 16, 15), // stomped
+            texture.NewSprite(334, 215, 16, 15), // shell dead
+            200,
+            50,
+            750
+        );
+        enemy[2] = new PiranhaPlant(
+            texture.NewAnimatedSprite(125, 180, 16, 23, 2, 0.2f),
+            250,  // X position (pipe location)
+            200   // Y position (top of pipe - adjust based on your screen)
+        );
+        enemy[3] = new Bowser(
+            texture.NewAnimatedSprite(255, 368, 35, 32, 4, 0.2f),
+            texture.NewAnimatedSprite(116, 368, 35, 32, 2, 0.2f),
+            fireSharedTexture,
+            161,   // Fire moving right - X coordinate (2 frames)
+            253,   // Fire moving right - Y coordinate
+            101,    // Fire moving left - X coordinate (2 frames)
+            253,    // Fire moving left - Y coordinate
+            24,             // Fire width
+            8,             // Fire height
+            100,            // Bowser Y position
+            50,             // Left bound
+            750             // Right bound
+        );
+        enemy[5] = new RotatingFireBar(
+            fireSharedTexture,
+            364,    // Fireball X position in sprite sheet
+            188,    // Fireball Y position in sprite sheet
+            8,    // Fireball width
+            8,    // Fireball height
+            400,  // Center X position on screen
+            300,  // Center Y position on screen
+            6,    // Number of fireballs
+            24    // Spacing between fireballs
+        );
+    }
+
+    private void LoadBlocks()
+    {
 
     }
 
     protected override void Update(GameTime gameTime){
-  
-}
+        Controller.Update(gameTime); // IControllers update function doesn't take parameters
+        Mario.Update(gameTime);
+        enemy[enemyIndex].Update(gameTime);
+        // block[blockIndex].Update(gameTime);
+    }
 
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        _spriteBatch.Begin();
-
-        goomba.Draw(_spriteBatch);
-        koopa.Draw(_spriteBatch);
-        piranhaPlant.Draw(_spriteBatch);
-        rotatingFireBar.Draw(_spriteBatch);
-        bowser.Draw(_spriteBatch);
-
-        //Code
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        enemy[enemyIndex].Draw(_spriteBatch);
+        // lblock[blockIndex].Draw(_spriteBatch);
         Mario.Draw(_spriteBatch);
         _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    public void incrementEnemy(){
+        enemyIndex = (enemyIndex + 1) % enemyArraySize;
+    }
+    public void decrementEnemy(){
+        enemyIndex = (enemyIndex - 1 + enemyArraySize) % enemyArraySize;
+    }
+    public void incrementBlock(){
+        blockIndex = (blockIndex + 1) % blockArraySize;
+    }
+    public void decrementBlock(){
+        blockIndex = (blockIndex - 1 + blockArraySize) % blockArraySize;
+    }
+    public void incrementItem(){
+        //TO DO
+    }
+    public void decrementItem(){
+        //TO DO
+    }
+    public void displayPowerUp(int index){
+        switch (index)
+        {
+            case 0:
+                Mario.ChangeState(new SmallMarioIdleState(Mario, texture, 0.15, 3));
+                break;
+            case 1:
+                Mario.ChangeState(new BigMarioIdleState(Mario, texture, 0.15, 3));
+                break;
+            case 2:
+                Mario.PowerUp(Power.FireFlower);
+                break;
+            case 3:
+                Mario.PowerUp(Power.Star);
+                break;
+        }
+    }
+    public void resetGame(){
+        displayPowerUp(0);
+        enemyIndex = 0;
+        blockIndex = 0;
     }
 }
 
