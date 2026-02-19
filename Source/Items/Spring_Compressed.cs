755631e55fec43ecc2222f997ae7dc62c50aedf6
TheRocker89 <perusekb@gmail.com>
2026-02-19 01:06:21 -0500

    Items v1
    Added the items. Only the fireflowers, and question blocks are fully functional, and the rest can be done pretty quickly but i wanted to make sure I have the main file working with the items for everyone else so I'm uploading it now.

---
+++ Source/Items/Spring_Compressed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MagicBrosMario.Source.Sprite;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MagicBrosMario.Source.Items
+{
+    internal class Spring_Compressed : IItems
+    {
+
+		public Spring_Compressed(SharedTexture texture)
+		{
+
+
+		}
+
+		public void Update(GameTime gameTime)
+		{
+
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+
+		}
+
+
+	}
+}
