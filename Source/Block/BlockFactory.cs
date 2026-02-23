95f3871a3e5f357aca8b526e72529f558281751b
jhuanglululu <huangchuangyun22@gmail.com>
2026-02-23 14:47:13 -0500

    added block to main class and adjust size

--- Source/Block/BlockFactory.cs
+++ Source/Block/BlockFactory.cs
@@ -5,7 +5,7 @@ namespace MagicBrosMario.Source.Block;
 
 public class BlockFactory
 {
-    public static readonly SharedTexture BLOCK_SHARED_TEXTURE = new SharedTexture();
+    private static readonly SharedTexture BLOCK_SHARED_TEXTURE = new SharedTexture();
     public const string TEXTURE_NAME = "blocks";
 
     public static void BindTexture(Texture2D texture)
