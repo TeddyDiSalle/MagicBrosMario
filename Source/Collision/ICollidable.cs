dde613f4b9cc1ee9efeb5914aa20c6b5c53ce859
jhuanglululu <huangchuangyun22@gmail.com>
2026-03-01 22:15:10 -0500

    added collision interface

---
+++ Source/Collision/ICollidable.cs
@@ -0,0 +1,15 @@
+using MagicBrosMario.Source.Block;
+using MagicBrosMario.Source.Items;
+using MagicBrosMario.Source.MarioStates;
+using Microsoft.Xna.Framework;
+
+namespace MagicBrosMario.Source.Collision;
+
+public interface ICollidable
+{
+    Rectangle CollisionArea { get; }
+    void OnCollidePlayer(Player player, CollideDirection direction); 
+    void OnCollideItem(IItems item, CollideDirection direction); 
+    void OnCollideEnemy(IEnemy enemy, CollideDirection direction); 
+    void OnCollideBlock(IBlock block, CollideDirection direction); 
+}
