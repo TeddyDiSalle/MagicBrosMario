18cbc1ecbe442375c2724a824f095cb63785c1c1
TheRocker89 <perusekb@gmail.com>
2026-03-04 22:28:38 -0500

    Update Level1.cs

---
+++ Source/Level/Level1.cs
@@ -0,0 +1,18 @@
+
+
+using System.Runtime.CompilerServices;
+using MagicBrosMario.Source;
+using Microsoft.Xna.Framework;
+
+public class Level1 : ILevel
+{
+    public void Update(GameTime gt)
+    {
+        // TODO
+    }
+
+    public void Draw(GameTime gt)
+    {
+        // TODO
+    }
+}
