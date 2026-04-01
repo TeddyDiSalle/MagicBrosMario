c8ff79485d0fd7c635d49c6ea8271434df2bcc22
Teddy <teddydisalle@gmail.com>
2026-04-01 14:52:10 -0400

    Mapping out the debug room
    Putting the arrays for items, enemies, blocks in the class so it is easy to edit for everybody

--- Source/Level/DebugRoom.cs
+++ Source/Level/DebugRoom.cs
@@ -13,12 +13,57 @@ namespace MagicBrosMario.Source.Level;
 
 public class DebugRoom : ILevel
 {
+	private string[] blockLines;
+	private string[] enemyLines;
+	private string[] itemLines;
+	private int levWidth;
+	private int levHeight;
+	private IBlock[][] blocks;
+	private IEnemy[][] enemies;
+	private IItems[][] items;
     public DebugRoom()	{
-		/*blockLines = File.ReadLines(Level1BlockCVS).ToArray();
-		enemyLines = File.ReadLines(Level1EnemyCVS).ToArray();
-		itemLines = File.ReadLines(Level1ItemCVS).ToArray();
+		blockLines =  ["  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,04,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,02,  ,  ,07,  ,07,  ",
+					   "  ,  ,  ,04,  ,  ,  ,02,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,02,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,02,  ,  ,  ,  ,  ,02",
+					   "02,02,02,02,02,02,02,02,02,02,02,02,02,02",];
 
-		
+		enemyLines =  ["  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,00,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,01,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",];
+
+		itemLines =  ["  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,00,  ,  ,  ", // one coin outside a mystery block
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,00,  ,01,  ", // coin inside a mystery block and mush/ff in a mb
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
+					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",];
+
+		levHeight = blockLines.Length;
+		if(enemyLines.Length != levHeight || itemLines.Length != levHeight)	{
+			throw new Exception("Level1: Enemy or Item CSV file has different height than Block CSV file");
+		}
+		levWidth = blockLines[0].Split(',').Length;
+		if(enemyLines[0].Split(',').Length != levWidth || itemLines[0].Split(',').Length != levWidth)	{
+			throw new Exception("Level1: Enemy or Item CSV file has different width than Block CSV file");
+		}
+
 		blocks = new IBlock[levHeight][];
 		enemies = new IEnemy[levHeight][];
 		items = new IItems[levHeight][];
@@ -27,7 +72,7 @@ public class DebugRoom : ILevel
 			enemies[i] = new IEnemy[levWidth];
 			items[i] = new IItems[levWidth];
 
-		}	*/
+		}
 	}
 	public void Initialize(Microsoft.Xna.Framework.Content.ContentManager contentManager, Texture2D tilesetTexture, Texture2D characterTexture, Texture2D itemTexture)
 	{
