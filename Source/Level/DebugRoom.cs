abbb6d0e1d882e6417d5dcb0a4ea53c50f90d9ea
Teddy <teddydisalle@gmail.com>
2026-04-03 14:08:29 -0400

    Refactoring Level
    Refactored level so that ParentLevel.cs does all the computation and stuff and DebugRoom.cs and Level1.cs just present their corresponding data.

--- Source/Level/DebugRoom.cs
+++ Source/Level/DebugRoom.cs
@@ -1,27 +1,13 @@
 // Made By Teddy
-using System;
-using System.IO;
-using System.Linq;
-using MagicBrosMario.Source.Block; 
-using MagicBrosMario.Source.Sprite; 
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using MagicBrosMario.Source.Collision;
-using MagicBrosMario.Source.Items;
 
 namespace MagicBrosMario.Source.Level;
 
-public class DebugRoom : ILevel
+public class DebugRoom : ParentLevel
 {
-	private string[] blockLines;
-	private string[] enemyLines;
-	private string[] itemLines;
-	private int levWidth;
-	private int levHeight;
-	private IBlock[][] blocks;
-	private IEnemy[][] enemies;
-	private IItems[][] items;
     public DebugRoom()	{
+
+		//If you wish to edit the debug room for your own purposes
+		// Look at Blocks, Items, or Enemies.xml in the LevelData folder for the correct object ids
 		blockLines =  ["  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
 					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
 					   "  ,  ,  ,  ,  ,  ,  ,04,  ,  ,  ,  ,  ,  ",
@@ -54,35 +40,5 @@ public class DebugRoom : ILevel
 					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
 					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",
 					   "  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ,  ",];
-
-		levHeight = blockLines.Length;
-		if(enemyLines.Length != levHeight || itemLines.Length != levHeight)	{
-			throw new Exception("Level1: Enemy or Item CSV file has different height than Block CSV file");
-		}
-		levWidth = blockLines[0].Split(',').Length;
-		if(enemyLines[0].Split(',').Length != levWidth || itemLines[0].Split(',').Length != levWidth)	{
-			throw new Exception("Level1: Enemy or Item CSV file has different width than Block CSV file");
-		}
-
-		blocks = new IBlock[levHeight][];
-		enemies = new IEnemy[levHeight][];
-		items = new IItems[levHeight][];
-		for(int i = 0; i < levHeight; i++)	{
-			blocks[i] = new IBlock[levWidth];
-			enemies[i] = new IEnemy[levWidth];
-			items[i] = new IItems[levWidth];
-
-		}
-	}
-	public void Initialize(Microsoft.Xna.Framework.Content.ContentManager contentManager, Texture2D tilesetTexture, Texture2D characterTexture, Texture2D itemTexture)
-	{
-	}
-
-	public void Update(GameTime gameTime)
-	{
-	}
-
-	public void Draw(SpriteBatch spriteBatch)
-	{
 	}
 }
