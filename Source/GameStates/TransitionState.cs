2f730287295fdb04e7ee64d99559fb43f8cf421a
TheRocker89 <perusekb@gmail.com>
2026-04-13 23:24:40 -0400

    r

--- Source/GameStates/TransitionState.cs
+++ Source/GameStates/TransitionState.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MagicBrosMario.Source.MarioStates;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MagicBrosMario.Source.GameStates
 {
@@ -13,13 +15,18 @@ namespace MagicBrosMario.Source.GameStates
         private ILevel _nextLevel;
         private Texture2D _transitionTexture;
         private float _timer = 1.5f;
+		private SpriteFont _font;
 
-        public TransitionState(MagicBrosMario game, ILevel nextLevel)
+
+		public TransitionState(MagicBrosMario game, ILevel nextLevel)
         {
             _game = game;
             _nextLevel = nextLevel;
             _transitionTexture = _game.Content.Load<Texture2D>("TransitionScreen");
-        }
+
+			_font = _game.Content.Load<SpriteFont>("Font");
+
+		}
 
         public void Update(GameTime gameTime)
         {
@@ -41,7 +48,15 @@ namespace MagicBrosMario.Source.GameStates
             spriteBatch.Draw(_transitionTexture, new Rectangle(0, 0,
                 _game.GraphicsDevice.Viewport.Width,
                 _game.GraphicsDevice.Viewport.Height), Color.White);
-        }
+
+
+			string text = "Lives: " + _game.Mario.Lives;
+
+			Vector2 textSize = _font.MeasureString(text);
+			Vector2 position = new Vector2((_game.GraphicsDevice.Viewport.Width / 2) - (textSize.X / 2), (_game.GraphicsDevice.Viewport.Height / 2));
+
+			spriteBatch.DrawString(_game.font, text, position, Color.White);
+		}
 
         public void Clear()
         {
