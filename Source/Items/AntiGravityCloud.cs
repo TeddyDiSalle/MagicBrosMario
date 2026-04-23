56b647411bbf7b7c4b7b5aee99a38c37ffbdce2c
TheRocker89 <perusekb@gmail.com>
2026-04-23 16:24:28 -0400

    cloud should be done

--- Source/Items/AntiGravityCloud.cs
+++ Source/Items/AntiGravityCloud.cs
@@ -8,118 +8,95 @@ using System;
 
 namespace MagicBrosMario.Source.Items
 {
-	internal class AntiGravityCloud : IItems, ICollidable
-	{
-		private const float GRAVITY_SPEED = 250f;
-		private const float RISE_SPEED = 30f;
-		private const float X_SPEED = 120f;
-		private Sprite.ISprite sprite;
-		private Vector2 floatPosition;
-		private int xDirection = 1;
-		private bool isCollected = false;
-		private bool hasRisen = false;
-		private bool isOnBlock = false;
-		private float riseAmount = 0f;
-		private float riseTarget = 32f;
+    internal class AntiGravityCloud : IItems, ICollidable
+    {
+        private Sprite.ISprite sprite;
 
-		public Rectangle CollisionBox
-		{
-			get
-			{
-				int width = (int)(sprite.Size.X - 4);
-				int height = sprite.Size.Y + 1;
-				return new Rectangle((int)floatPosition.X + 2, (int)floatPosition.Y, width, height);
-			}
-		}
+        private const float xSpeed = 100f;
+        private const float riseSpeed = 25f; 
 
-		public Point position { get => sprite.Position; private set => sprite.Position = value; }
+        private const float bobSpeed = 4f;
+        private const float bobDist = 25f;
 
-		public AntiGravityCloud(SharedTexture texture, int positionX, int positionY)
-		{
-			sprite = texture.NewSprite(184, 34, 16, 16);
-			sprite.Scale = 2f;
-			floatPosition = new Vector2(positionX, positionY);
-			sprite.Position = floatPosition.ToPoint();
-			CollisionController.Instance.AddItem(this);
-		}
+        private Vector2 floatPosition;
+        private float centerPoint;
+        private float totalTime = 0f;
 
-		public void Update(GameTime gameTime)
-		{
-			if (isCollected) return;
-			float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
-			if (!hasRisen)
-			{
-				Rise(dt);
-			}
-			else
-			{
-				if (!isOnBlock)
-				{
-					floatPosition.Y += (GRAVITY_SPEED * dt);
-				}
-				floatPosition.X += (xDirection * X_SPEED * dt);
-			}
-			sprite.Position = floatPosition.ToPoint();
-			isOnBlock = false;
-		}
+        private bool isCollected = false;
+        private bool hasRisen = false;
+        private float riseAmount = 0f;
+        private float riseTarget = 32f;
 
-		private void Rise(float dt)
-		{
-			float step = RISE_SPEED * dt;
-			floatPosition.Y -= step;
-			riseAmount += step;
-			if (riseAmount >= riseTarget)
-			{
-				hasRisen = true;
-			}
-		}
+        public Rectangle CollisionBox
+        {
+            get
+            {
+                return new Rectangle((int)floatPosition.X + 2, (int)floatPosition.Y, sprite.Size.X, sprite.Size.Y);
+            }
+        }
 
-		public void Draw(SpriteBatch spriteBatch)
-		{
-			if (!isCollected)
-			{
-				sprite.Draw(spriteBatch);
-			}
-		}
+        public Point position { get => sprite.Position; private set => sprite.Position = value; }
 
-		public void OnCollideBlock(IBlock block, CollideDirection direction)
-		{
-			if (hasRisen)
-			{
-				Rectangle blockBox = block.CollisionBox;
-				if (direction == CollideDirection.Down)
-				{
-					isOnBlock = true;
-					floatPosition.Y = blockBox.Top - sprite.Size.Y;
-				}
-				else if (direction == CollideDirection.Left)
-				{
-					xDirection = 1;
-					floatPosition.X = blockBox.Right;
-				}
-				else if (direction == CollideDirection.Right)
-				{
-					xDirection = -1;
-					floatPosition.X = blockBox.Left - sprite.Size.X;
-				}
-				else if (direction == CollideDirection.Top)
-				{
-					floatPosition.Y = blockBox.Bottom;
-				}
-				sprite.Position = floatPosition.ToPoint();
-			}
-		}
+        public AntiGravityCloud(SharedTexture texture, int positionX, int positionY)
+        {
+            sprite = texture.NewSprite(95, 118, 16, 13);
+            sprite.Scale = 2f;
 
-		public void OnCollidePlayer(Player player, CollideDirection direction)
-		{
-			if (isCollected) return;
-			isCollected = true;
-			CollisionController.Instance.RemoveItem(this);
-			sprite.Drop();
-		}
+            floatPosition = new Vector2(positionX, positionY);
+            centerPoint = positionY;
 
-		public void OnCollideItem(IItems item, CollideDirection direction) { }
-		public void OnCollideEnemy(IEnemy enemy, CollideDirection direction) { }
-		public bool getCollected() => isCollected;
-	}
+            sprite.Position = floatPosition.ToPoint();
+            CollisionController.Instance.AddItem(this);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (isCollected) return;
+
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (!hasRisen)
+            {
+                float riseStep = 40f * dt;
+                floatPosition.Y -= riseStep;
+                riseAmount += riseStep;
+                if (riseAmount >= riseTarget)
+                {
+                    hasRisen = true;
+                    centerPoint = floatPosition.Y;
+                }
+            }
+            else
+            {
+                totalTime += dt;
+
+                floatPosition.X += xSpeed * dt;
+                centerPoint -= riseSpeed * dt;
+
+                float sineOffset = (float)Math.Sin(totalTime * bobSpeed) * bobDist;
+
+                floatPosition.Y = centerPoint + sineOffset;
+            }
+
+            sprite.Position = floatPosition.ToPoint();
+            sprite.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (!isCollected) sprite.Draw(spriteBatch);
+        }
+
+        public void OnCollidePlayer(Player player, CollideDirection direction)
+        {
+            isCollected = true;
+            CollisionController.Instance.RemoveItem(this);
+            sprite.Drop();
+        }
+        public void OnCollideBlock(IBlock block, CollideDirection direction) { }
+        public void OnCollideItem(IItems item, CollideDirection direction) { }
+        public void OnCollideEnemy(IEnemy enemy, CollideDirection direction) { }
+
+        public bool getCollected() => isCollected;
+    }
 }
