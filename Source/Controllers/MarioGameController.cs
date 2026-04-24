55c0040d108fe3c6f8c4ffcf7e6e1674873a8a37
Teddy <teddydisalle@gmail.com>
2026-04-24 14:14:20 -0400

    Temporary fix ofr mario moving left and right

--- Source/Controllers/MarioGameController.cs
+++ Source/Controllers/MarioGameController.cs
@@ -96,7 +96,7 @@ public static class MarioGameController{
     {
         if(!muted){
             MNKUpdate(gameTime);
-            GPUpdate(gameTime);
+            //GPUpdate(gameTime);
         }
     }
 
