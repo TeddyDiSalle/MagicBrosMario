21024fa876ba2ed3cf2ea59d2453bd611719087c
TheRocker89 <perusekb@gmail.com>
2026-04-26 21:45:31 -0400

    t

--- Source/Enemies/RotatingFireBar.cs
+++ Source/Enemies/RotatingFireBar.cs
@@ -13,7 +13,7 @@ public class RotatingFireBar : IEnemy, ICollidable
 {
     public bool AlwaysActive => true;
     private const float ROTATION_SPEED = 1.0f;
-    private const float SCALE = 4f;
+    private const float SCALE = 2f;
 
     private Sprite.ISprite[] fireballs;
     private readonly Point centerPosition;
