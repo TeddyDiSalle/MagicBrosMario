775fb47d86b4ea20bc33adce9c3497ae573a5cb8
Teddy <teddydisalle@gmail.com>
2026-05-01 16:42:34 -0400

    ParentLevel refactor

--- Source/Level/ParentLevel.cs
+++ Source/Level/ParentLevel.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using MagicBrosMario.Source.Block;
 using MagicBrosMario.Source.Collision;
 using MagicBrosMario.Source.HUDAndScoring;
@@ -42,19 +41,27 @@ public abstract class ParentLevel : ILevel
     protected string[] enemyLines;
     protected string[] itemLines;
 
+    private string[][] blockCsv;
+    private string[][] enemyCsv;
+    private string[][] itemCsv;
+
     private int levWidth;
     private int levHeight;
+
     protected List<Point> checkpointPositions = new List<Point>();
 
     public Point MarioStartPos { get; protected set; }
     public string Name { get; protected set; }
     public int TimeLimit { get; protected set; }
 
+    private const string AxeItemId = "06";
+    private const string BridgeBlockId = "16";
+
     private readonly DeferredPipeLinkResolver _pipeResolver = new();
     private readonly HashSet<IEnemy> _activatedEnemies = new();
+
     private int bridgeGroupCounter = 0;
     private int bridgeOrderCounter = 1;
-
     private ISprite backgroundSprite;
 
     public void Initialize(
@@ -63,64 +70,19 @@ public abstract class ParentLevel : ILevel
         Texture2D eTexture,
         Texture2D iTexture)
     {
+        _ = contentManager;
+
         if (MagicBrosMario.INSTANCE.MarioStartPosition.X == -1)
         {
             MagicBrosMario.INSTANCE.MarioStartPosition = MarioStartPos;
         }
 
+        EnsureCsvArraysAreLoaded();
         ValidateCsvDimensions();
         InitializeLevelArrays();
-
-        SoundController.PlayMusic(backgroundMusic, volume);
-
-        HUD.Instance.LevelStart();
-        HUD.Instance.SetTime(TimeLimit);
-
+        InitializeLevelState();
         InitializeManagers(bTexture, eTexture, iTexture);
         LoadContent();
-    }
-
-    private void ValidateCsvDimensions()
-    {
-        levHeight = blockLines.Length;
-
-        if (enemyLines.Length != levHeight || itemLines.Length != levHeight)
-        {
-            throw new Exception("Level: Enemy or Item CSV file has different height than Block CSV file");
-        }
-
-        levWidth = blockLines[0].Split(',').Length;
-
-        if (enemyLines[0].Split(',').Length != levWidth || itemLines[0].Split(',').Length != levWidth)
-        {
-            throw new Exception(
-                $"Level: Enemy or Item CSV file has different width than Block CSV file {levWidth} vs {enemyLines[0].Split(',').Length} vs {itemLines[0].Split(',').Length}");
-        }
-    }
-
-    private void InitializeLevelArrays()
-    {
-        blocks = new IBlock[levHeight][];
-        enemies = new IEnemy[levHeight][];
-        items = new IItems[levHeight][];
-
-        for (int i = 0; i < levHeight; i++)
-        {
-            blocks[i] = new IBlock[levWidth];
-            enemies[i] = new IEnemy[levWidth];
-            items[i] = new IItems[levWidth];
-        }
-    }
-
-    private static void InitializeManagers(Texture2D bTexture, Texture2D eTexture, Texture2D iTexture)
-    {
-        if (_managersInitialized)
-            return;
-
-        _managersInitialized = true;
-        BlockManager.Initialize(bTexture);
-        EnemyManager.Initialize(eTexture);
-        ItemManager.Initialize(iTexture);
     }
 
     public void ReadFromCSV()
@@ -128,12 +90,83 @@ public abstract class ParentLevel : ILevel
         blockLines = File.ReadLines(Level1BlockCVS).ToArray();
         enemyLines = File.ReadLines(Level1EnemyCVS).ToArray();
         itemLines = File.ReadLines(Level1ItemCVS).ToArray();
+
+        blockCsv = ParseCsvLines(blockLines);
+        enemyCsv = ParseCsvLines(enemyLines);
+        itemCsv = ParseCsvLines(itemLines);
+    }
+
+    public void Update(GameTime gt)
+    {
+        UpdateCheckpoints();
+        UpdateEnemies(gt);
+        UpdateItems(gt);
+        UpdateBlocks(gt);
+    }
+
+    public void Draw(SpriteBatch sb) { }
+
+    public void AddItem(IItems item)
+    {
+        for (int r = 0; r < levHeight; r++)
+        {
+            for (int c = 0; c < levWidth; c++)
+            {
+                if (items[r][c] != null)
+                    continue;
+
+                items[r][c] = item;
+                CollisionController.Instance.AddItem(item);
+                return;
+            }
+        }
+
+        throw new Exception("No empty slot available to add item");
+    }
+
+    public void Clear()
+    {
+        _activatedEnemies.Clear();
+        CollisionController.Instance.RemoveAll();
+
+        blocks = [];
+        items = [];
+        enemies = [];
+
+        backgroundSprite?.Drop();
+        backgroundSprite = null;
+
+        HUD.Instance.LevelOver();
+        SoundController.StopMusic();
+    }
+
+    protected void LoadContent()
+    {
+        if (BackgroundName != null)
+        {
+            LoadBackground();
+        }
+
+        _pipeResolver.Clear();
+
+        ForEachCell((row, col) =>
+        {
+            LevelCellToken token = LevelCellTokenParser.ParseBlockToken(blockCsv[row][col]);
+            string itemId = itemCsv[row][col];
+            string enemyId = enemyCsv[row][col];
+
+            LoadItemCell(token, itemId, row, col);
+            LoadBlockCell(token, itemId, row, col);
+            LoadEnemyCell(enemyId, row, col);
+        });
+
+        _pipeResolver.FinalizeLinks(blocks, tileSize);
     }
 
     protected void LoadBackground()
     {
-        Texture2D backgroundTexture =
-            MagicBrosMario.INSTANCE.Content.Load<Texture2D>("LevelData/" + Name + "/" + BackgroundName);
+        Texture2D backgroundTexture = MagicBrosMario.INSTANCE.Content.Load<Texture2D>(
+            "LevelData/" + Name + "/" + BackgroundName);
 
         backWidth = backgroundTexture.Width;
         backHeight = backgroundTexture.Height;
@@ -147,86 +180,190 @@ public abstract class ParentLevel : ILevel
         backgroundSprite.Background();
     }
 
-    public void Update(GameTime gt)
+    private void InitializeLevelState()
     {
-        Rectangle activationBounds = GetEnemyActivationBounds();
-        for (int i = 0; i < checkpointPositions.Count; i++)
+        SoundController.PlayMusic(backgroundMusic, volume);
+        HUD.Instance.LevelStart();
+        HUD.Instance.SetTime(TimeLimit);
+    }
+
+    private void EnsureCsvArraysAreLoaded()
+    {
+        if (blockCsv == null && blockLines != null)
         {
-            if (MagicBrosMario.INSTANCE.Mario.Position.X > checkpointPositions[i].X)
-            {
-                //Console.WriteLine("Checkpoint reached at posdition: " + checkpointPositions[i]);
-                MagicBrosMario.INSTANCE.MarioStartPosition = checkpointPositions[i];
-                checkpointPositions.RemoveAt(i);
-            }
+            blockCsv = ParseCsvLines(blockLines);
         }
 
-
-        for (int r = 0; r < levHeight; r++)
+        if (enemyCsv == null && enemyLines != null)
         {
-            for (int c = 0; c < levWidth; c++)
+            enemyCsv = ParseCsvLines(enemyLines);
+        }
+
+        if (itemCsv == null && itemLines != null)
+        {
+            itemCsv = ParseCsvLines(itemLines);
+        }
+
+        if (blockCsv == null || enemyCsv == null || itemCsv == null)
+        {
+            throw new Exception("Level CSV data has not been loaded. Call ReadFromCSV before Initialize.");
+        }
+    }
+
+    private static string[][] ParseCsvLines(string[] lines)
+    {
+        return lines
+            .Select(line => line.Split(',').Select(cell => cell.Trim()).ToArray())
+            .ToArray();
+    }
+
+    private void ValidateCsvDimensions()
+    {
+        levHeight = blockCsv.Length;
+
+        if (levHeight == 0)
+        {
+            throw new Exception("Level: Block CSV file is empty");
+        }
+
+        levWidth = blockCsv[0].Length;
+
+        if (levWidth == 0)
+        {
+            throw new Exception("Level: Block CSV file has no columns");
+        }
+
+        ValidateMatchingHeight(enemyCsv, "Enemy");
+        ValidateMatchingHeight(itemCsv, "Item");
+
+        ValidateMatchingWidth(blockCsv, "Block");
+        ValidateMatchingWidth(enemyCsv, "Enemy");
+        ValidateMatchingWidth(itemCsv, "Item");
+    }
+
+    private void ValidateMatchingHeight(string[][] csv, string csvName)
+    {
+        if (csv.Length != levHeight)
+        {
+            throw new Exception(
+                $"Level: {csvName} CSV file has different height than Block CSV file. Expected {levHeight}, got {csv.Length}");
+        }
+    }
+
+    private void ValidateMatchingWidth(string[][] csv, string csvName)
+    {
+        for (int row = 0; row < csv.Length; row++)
+        {
+            if (csv[row].Length != levWidth)
             {
-                if (enemies[r][c] != null)
-                {
-                    IEnemy enemy = enemies[r][c];
-
-                    if (enemy.AlwaysActive || _activatedEnemies.Contains(enemy) ||
-                        ShouldActivateEnemy(enemy, activationBounds))
-                    {
-                        if (_activatedEnemies.Add(enemy))
-                            CollisionController.Instance.AddEnemy(enemy);
-                        enemy.Update(gt);
-                    }
-                }
-
-                if (items[r][c] != null)
-                {
-                    items[r][c].Update(gt);
-                    if (items[r][c].getCollected())
-                    {
-                        Collision.CollisionController.Instance.RemoveItem(items[r][c]);
-                        items[r][c] = null;
-                    }
-                }
-
-                if (blocks[r][c] != null)
-                {
-                    blocks[r][c].Update(gt);
-                }
+                throw new Exception(
+                    $"Level: {csvName} CSV row {row} has different width than Block CSV file. Expected {levWidth}, got {csv[row].Length}");
             }
         }
     }
 
-    public void Draw(SpriteBatch sb) { }
-
-    protected void LoadContent()
+    private void InitializeLevelArrays()
     {
-        if (BackgroundName != null)
+        blocks = CreateGrid<IBlock>();
+        enemies = CreateGrid<IEnemy>();
+        items = CreateGrid<IItems>();
+    }
+
+    private T[][] CreateGrid<T>()
+    {
+        T[][] grid = new T[levHeight][];
+
+        for (int row = 0; row < levHeight; row++)
         {
-            LoadBackground();
+            grid[row] = new T[levWidth];
         }
 
-        _pipeResolver.Clear();
+        return grid;
+    }
 
-        for (int r = 0; r < levHeight; r++)
+    private static void InitializeManagers(Texture2D bTexture, Texture2D eTexture, Texture2D iTexture)
+    {
+        if (_managersInitialized)
+            return;
+
+        _managersInitialized = true;
+        BlockManager.Initialize(bTexture);
+        EnemyManager.Initialize(eTexture);
+        ItemManager.Initialize(iTexture);
+    }
+
+    private void ForEachCell(Action<int, int> action)
+    {
+        for (int row = 0; row < levHeight; row++)
         {
-            string[] blockIds = blockLines[r].Split(',');
-            string[] enemyIds = enemyLines[r].Split(',');
-            string[] itemIds = itemLines[r].Split(',');
-
-            for (int c = 0; c < levWidth; c++)
+            for (int col = 0; col < levWidth; col++)
             {
-                //Console.WriteLine($"Loading cell at row {r}, col {c} with blockId '{blockIds[c]}', enemyId '{enemyIds[c]}', itemId '{itemIds[c]}'");
-                LevelCellToken token = LevelCellTokenParser.ParseBlockToken(blockIds[c]);
-                string enemyId = enemyIds[c].Trim();
-                string itemId = itemIds[c].Trim();
-
-                LoadItemCell(token, itemId, r, c);
-                LoadBlockCell(token, itemId, r, c);
-                LoadEnemyCell(enemyId, r, c);
+                action(row, col);
             }
         }
+    }
 
-        _pipeResolver.FinalizeLinks(blocks, tileSize);
+    private void UpdateCheckpoints()
+    {
+        for (int i = checkpointPositions.Count - 1; i >= 0; i--)
+        {
+            if (MagicBrosMario.INSTANCE.Mario.Position.X <= checkpointPositions[i].X)
+                continue;
+
+            MagicBrosMario.INSTANCE.MarioStartPosition = checkpointPositions[i];
+            checkpointPositions.RemoveAt(i);
+        }
+    }
+
+    private void UpdateEnemies(GameTime gt)
+    {
+        Rectangle activationBounds = GetEnemyActivationBounds();
+
+        ForEachCell((row, col) =>
+        {
+            IEnemy enemy = enemies[row][col];
+
+            if (enemy == null || !IsEnemyActive(enemy, activationBounds))
+                return;
+
+            if (_activatedEnemies.Add(enemy))
+            {
+                CollisionController.Instance.AddEnemy(enemy);
+            }
+
+            enemy.Update(gt);
+        });
+    }
+
+    private void UpdateItems(GameTime gt)
+    {
+        ForEachCell((row, col) =>
+        {
+            IItems item = items[row][col];
+
+            if (item == null)
+                return;
+
+            item.Update(gt);
+
+            if (!item.getCollected())
+                return;
+
+            CollisionController.Instance.RemoveItem(item);
+            items[row][col] = null;
+        });
+    }
+
+    private void UpdateBlocks(GameTime gt)
+    {
+        ForEachCell((row, col) => blocks[row][col]?.Update(gt));
+    }
+
+    private bool IsEnemyActive(IEnemy enemy, Rectangle activationBounds)
+    {
+        return enemy.AlwaysActive
+            || _activatedEnemies.Contains(enemy)
+            || enemy.CollisionBox.Intersects(activationBounds);
     }
 
     private void LoadItemCell(LevelCellToken token, string itemId, int row, int col)
@@ -237,32 +374,26 @@ public abstract class ParentLevel : ILevel
             return;
         }
 
-        if (string.IsNullOrEmpty(token.BlockId))
-        {
-            int? group = null;
-            if (itemId == "06")
-            {
-                // axe has appeared so there is a new group of bridge blocks coming up
-                bridgeGroupCounter++;
-                bridgeOrderCounter = 1;
-                group = bridgeGroupCounter;
-            }
+        if (!string.IsNullOrEmpty(token.BlockId))
+            return;
 
-            items[row][col] = ItemManager.CreateItem(itemId, col * tileSize, row * tileSize, group);
+        int? group = null;
+
+        if (itemId == AxeItemId)
+        {
+            bridgeGroupCounter++;
+            bridgeOrderCounter = 1;
+            group = bridgeGroupCounter;
         }
+
+        items[row][col] = ItemManager.CreateItem(itemId, col * tileSize, row * tileSize, group);
     }
 
     private void LoadBlockCell(LevelCellToken token, string itemId, int row, int col)
     {
         if (string.IsNullOrEmpty(token.BlockId))
         {
-            blocks[row][col] = null;
-
-            if (!string.IsNullOrEmpty(token.PipeLabel))
-            {
-                _pipeResolver.RegisterMarker(row, col, token.PipeLabel);
-            }
-
+            LoadEmptyBlockCell(token, row, col);
             return;
         }
 
@@ -272,23 +403,22 @@ public abstract class ParentLevel : ILevel
             return;
         }
 
-        int? group = null;
-        int? order = null;
-        if (token.BlockId == "16"){
-            // bridge block
-            if (!(blocks[row][col - 1] is BridgeBlock))
-            {
-                // new bridge group
-                //We take care of a new bridge group when the axe shows up on row-1
-            } else
-            {
-                // there is already a bridge block to the left, same group
-                bridgeOrderCounter++;
-            }
+        CreateRegularBlock(token, itemId, row, col);
+    }
 
-            group = bridgeGroupCounter;
-            order = bridgeOrderCounter;
+    private void LoadEmptyBlockCell(LevelCellToken token, int row, int col)
+    {
+        blocks[row][col] = null;
+
+        if (!string.IsNullOrEmpty(token.PipeLabel))
+        {
+            _pipeResolver.RegisterMarker(row, col, token.PipeLabel);
         }
+    }
+
+    private void CreateRegularBlock(LevelCellToken token, string itemId, int row, int col)
+    {
+        GetBridgeData(token.BlockId, row, col, out int? group, out int? order);
 
         blocks[row][col] = BlockManager.CreateBlock(
             token.BlockId,
@@ -301,6 +431,25 @@ public abstract class ParentLevel : ILevel
         );
 
         CollisionController.Instance.AddBlock(blocks[row][col]);
+    }
+
+    private void GetBridgeData(string blockId, int row, int col, out int? group, out int? order)
+    {
+        group = null;
+        order = null;
+
+        if (blockId != BridgeBlockId)
+            return;
+
+        bool hasBridgeOnLeft = col > 0 && blocks[row][col - 1] is BridgeBlock;
+
+        if (hasBridgeOnLeft)
+        {
+            bridgeOrderCounter++;
+        }
+
+        group = bridgeGroupCounter;
+        order = bridgeOrderCounter;
     }
 
     private void LoadEnemyCell(string enemyId, int row, int col)
@@ -327,46 +476,4 @@ public abstract class ParentLevel : ILevel
             Camera.Instance.WindowSize.Y + (2 * verticalBufferTiles) * tileSize
         );
     }
-
-    private bool ShouldActivateEnemy(IEnemy enemy, Rectangle activationBounds)
-    {
-        return enemy.CollisionBox.Intersects(activationBounds);
-    }
-
-    public void AddItem(IItems item)
-    {
-        for (int r = 0; r < levHeight; r++)
-        {
-            for (int c = 0; c < levWidth; c++)
-            {
-                //Console.WriteLine($"Trying to add item at row {r}, col {c}");
-                if (items[r][c] == null)
-                {
-                    items[r][c] = item;
-                    CollisionController.Instance.AddItem(item);
-                    return;
-                }
-            }
-        }
-
-        throw new Exception("No empty slot available to add item");
-    }
-
-    public void Clear()
-    {
-        _activatedEnemies.Clear();
-        CollisionController.Instance.RemoveAll();
-        blocks = [];
-        items = [];
-        enemies = [];
-        if (backgroundSprite != null)
-        {
-            backgroundSprite.Drop();
-            backgroundSprite = null;
-        }
-
-        HUD.Instance.LevelOver();
-        SoundController.StopMusic();
-    }
-
 }
